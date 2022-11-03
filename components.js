e78d8012ad0b0702fb26b30180823df89545a452
Vedant Mali <89029656+vedantmali05@users.noreply.github.com>
2022-11-03 19:37:14 +0530

    v1

--- components.js
+++ components.js
@@ -1,12 +1,25 @@
-// Function that populates an Icon SVG Code where the element with class is found
-function populateIcon(icon, parent) {
-    for (let i = 0; i < parent.length; i++) {
-        parent[i].innerHTML = icon;
-    }
-}
+// // Function that populates an Icon SVG Code where the element with class is found
+// function populateIcon(icon, parent) {
+//     for (let i = 0; i < parent.length; i++) {
+//         parent[i].innerHTML = icon;
+//     }
+// }
 
-// Right Arrow
-let iconParent_arrowRight = document.querySelectorAll(".icon.arrow-right");
-let icon_arrowRight = 
-`<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 15 15"><path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/></svg>`
-populateIcon(icon_arrowRight, iconParent_arrowRight);
+// // Right Arrow
+// let iconParent_arrowRight = document.querySelectorAll(".icon.arrow-right");
+// let icon_arrowRight = 
+// `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 15 15"><path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/></svg>`
+// populateIcon(icon_arrowRight, iconParent_arrowRight);
+
+
+// ///////////////////// Menu Button
+
+let menuBtnArr = document.getElementsByClassName("menu-btn-btn");
+let menuBtnMenuArr = document.getElementsByClassName("menu-btn-menu");
+
+for (let i = 0; i < menuBtnArr.length; i++) {
+    menuBtnArr[i].addEventListener("click", function () {
+        menuBtnArr[i].classList.toggle("menu-open");
+        menuBtnMenuArr[i].classList.toggle("menu-open");
+    })
+}
